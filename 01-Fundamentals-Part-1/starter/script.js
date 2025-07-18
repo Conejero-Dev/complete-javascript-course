a8e200b7e56f6f36d6f9895d9eb6b677f57b6315
Javier Fajardo <20287019+Conejero-Dev@users.noreply.github.com>
2025-07-18 23:06:17 +0100

    Values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,25 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Javi');
+console.log(23);
+
+let firstName = 'Javi';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
